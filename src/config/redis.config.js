e466f6ca5b20ed922b5cc3f2321c28b5851f0305
혜정 <jung5318@gmail.com>
2023-02-20 03:23:15 +0900

    ㅐㄱ같은 레디스

--- src/config/redis.config.js
+++ src/config/redis.config.js
@@ -1,6 +1,12 @@
 import { createClient } from 'redis';
 
-const client = createClient();
+const client = createClient({
+  socket: {
+    port: 6379,
+    host: '3.35.127.36',
+  },
+  legacyMode: true, // 반드시 설정 !!
+});
 
 client.on('error', (err) => console.log('Redis Client Error', err));
 
